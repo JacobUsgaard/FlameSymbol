d833408a5040fed918d815b81e4ca667729c9c73
JacobUsgaard <jacob.usgaard@outlook.com>
2021-02-07 10:40:44 -0600

    Add AttaskDetailPanel tests for code coverage

---
+++ Assets/Tests/UI/AttackDetailPanelTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+namespace Tests.UI
+{
+    public class AttackDetailPanelTests : GameManagerTest
+    {
+        /// <summary>
+        /// Validate that the AssistDetailPanel shows up on the left when the
+        /// characters are on the right side of the screen
+        /// </summary>
+        /// <returns></returns>
+        [UnityTest]
+        public IEnumerator ValidateLeftPlacement()
+        {
+            Character sourceCharacter = GameManager.CurrentLevel.GetCharacter(2, 2);
+            sourceCharacter.Items.Clear();
+            sourceCharacter.Items.Add(IronSword.Create());
+            sourceCharacter.AddProficiency(new Proficiency(typeof(Sword), Proficiency.Rank.A));
+
+            Character targetCharacter = GameManager.CurrentLevel.GetCharacter(2, 1);
+
+            GameManager.AttackDetailPanel.Show(sourceCharacter, targetCharacter);
+
+            Assert.True(GameManager.AttackDetailPanel.gameObject.activeSelf);
+            yield return null;
+        }
+
+        /// <summary>
+        /// Validate that the AssistDetailPanel shows up on the right when the
+        /// characters are on the left side of the screen
+        /// </summary>
+        /// <returns></returns>
+        [UnityTest]
+        public IEnumerator ValidateRightPlacement()
+        {
+            Character sourceCharacter = GameManager.CurrentLevel.GetCharacter(0, 0);
+            sourceCharacter.Items.Clear();
+            sourceCharacter.Items.Add(IronSword.Create());
+            sourceCharacter.AddProficiency(new Proficiency(typeof(Sword), Proficiency.Rank.A));
+
+            Character targetCharacter = GameManager.CurrentLevel.GetCharacter(0, 1);
+
+            GameManager.AttackDetailPanel.Show(sourceCharacter, targetCharacter);
+
+            Assert.True(GameManager.AttackDetailPanel.gameObject.activeSelf);
+            yield return null;
+        }
+    }
+}
